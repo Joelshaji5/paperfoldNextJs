406000e222cca42d9e7d45d7919eba89f6cdb1c9
joelshaji5 <joel.shaji15@gmail.com>
2023-10-31 14:57:09 +0530

    Paperfold minor ui change

--- src/app/Component/NavBar/Navbar.jsx
+++ src/app/Component/NavBar/Navbar.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
+import "../../Style/NavBar.css"
 
 const StickyNavbar = () => {
   const [isSticky, setIsSticky] = useState(false);
