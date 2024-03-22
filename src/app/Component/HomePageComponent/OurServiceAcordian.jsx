42635aeb8ed577f9979d76235aa75a45aa872b80
joel <joel.shaji15@gmail.com>
2024-03-22 13:14:50 +0530

    mobile fix

--- src/app/Component/HomePageComponent/OurServiceAcordian.jsx
+++ src/app/Component/HomePageComponent/OurServiceAcordian.jsx
@@ -1,91 +1,12 @@
 import React, { useState } from "react";
+import servicesData from "../Constent/services.json"; // Import JSON data
 import "../../Style/OurServiceAcordian.css";
 
 const OurServiceAccordion = () => {
   const [active, setActive] = useState(3);
 
-  const artists = [
-    {
-      name: "Web Development",
-      description: `We offer a wide range of web development services, including front-end development, back-end development, and full-stack development. We use the latest technologies and best practices to create websites that are both visually appealing and functional. We also offer web design, web programming, web security, and web accessibility services. We are committed to providing our clients with the highest quality web development services.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC1.png"
-    },
-
-    {
-      name: "UI/UX Design",
-      description: `We design user interfaces and user experiences that are both visually appealing and functional. We understand the importance of creating a seamless user experience that meets the needs of your target audience. Our team of experienced UI/UX designers will work with you to understand your goals and create a design that will help you achieve them.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC2.png"
-    },
-    {
-      name: "Digital Marketing",
-      description: `We offer a comprehensive range of digital marketing services, including search engine optimization (SEO), pay-per-click (PPC) advertising, social media marketing, content marketing, and email marketing. We can help you reach your target audience, increase brand awareness, and generate leads and sales. We are experts in using digital marketing to achieve your business goals.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC3.png"
-    },
-    {
-      name: "Cloud / Database Management",
-      description: `We offer a wide range of cloud database management services to help you get the most out of your data. Our team of experts can help you choose the right cloud database for your needs, set it up and configure it, and manage it on an ongoing basis. We also offer a variety of other database management services, such as data migration, backup and recovery, and performance tuning.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC4.png"
-    },{
-      name: "Software Development",
-      description: `We offer a wide range of software development services, including web development, mobile app development, software engineering, data science, and machine learning. We also offer a variety of managed services, so you can focus on your business while we take care of your software needs.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC5.png"
-    },
-    {
-      name: "Branding",
-      description: `Branding & Logo Creating a strong positive perception of a company, its products or services in the customer's mind by combining such elements as logo, design, mission statement, and a consistent theme throughout all marketing & Communications.`,
-      spotify: {
-        profileUrl:
-          "https://open.spotify.com/artist/205i7E8fNVfojowcQSfK9m?si=HidnJ9jEQ5aK-dKZEZWdLw",
-        embedUrl:
-          "https://open.spotify.com/embed/artist/205i7E8fNVfojowcQSfK9m?utm_source=generator",
-      },
-      backgroundUrl: "/ServiceC6.png"
-    },
-    {
-      name: "Google Services",
-      description: `Google Ads: Google Ads is a pay-per-click (PPC) advertising platform that allows businesses to display their ads on Google Search and other Google properties.
-      Google Merchant Center: Google Merchant Center is a free service that allows businesses to upload their product catalogs to Google. 
-      Google Analytics: Google Analytics is a web analytics service that helps businesses track and measure website traffic.
-      Google Maps: Google Maps is a mapping service that can help businesses to find and reach their customers.`,
-
-      backgroundUrl: "/ServiceC7.png"
-    },
-
-    // Add more artist data here
-  ];
-
   const nextSlide = () => {
-    if (active < artists.length - 1) {
+    if (active < servicesData.length - 1) {
       setActive(active + 1);
     }
   };
@@ -102,38 +23,38 @@ const OurServiceAccordion = () => {
         <h1 className="Servicetxt">Our Services</h1>
       </div>
 
-      <div className=" HomeCDiv     h-[523px] ServiceCardDiv mr-5 ">
-        <ul className="h-[99%]  mb-20 Serviceul">
-          {artists.map((artist, i) => (
-            <li 
-              
+      <div className=" HomeCDiv h-[523px] ServiceCardDiv mr-5 ">
+        <ul className="h-[99%] mb-20 Serviceul">
+          {servicesData.map((service, i) => (
+            <li
               key={i}
-              style={{
-                backgroundImage: `url(${artist.backgroundUrl});    
-            background-position: center;
-`,
+              style
+              ={{
+                backgroundImage: `url(${service.backgroundUrl})`,
+                backgroundPosition: "center",
               }}
               role="button"
               className={active === i ? "activeService" : ""}
               onClick={() => setActive(i)}
-            >
-              <h3 className="cardCloseService">{artist.name}</h3>
-                <div className="inner section-content">
-                  <div className="bio gap-5">
-                    <h2 className="AccordianH2">{artist.name}</h2>
-                    <p className="AccordianP">{artist.description}</p>
-                  </div>
+              >
+              <h3 className="cardCloseService">{service.name}</h3>
+              <div className="inner section-content">
+                <div className="bio gap-5">
+                  <h2 className="AccordianH2">{service.name}</h2>
+                  <p className="AccordianP">{service.description}</p>
+                </div>
               </div>
-            </li>
-          ))}
-        </ul>
-        {/* <button onClick={prevSlide}>Previous</button>
-      <button onClick={nextSlide}>Next</button> */}
-      </div>
-
-      <div></div>
-    </div>
-  );
-};
-
-export default OurServiceAccordion;
+              </li>
+              ))}
+              </ul>
+              {/* <button onClick={prevSlide}>Previous</button>
+              <button onClick={nextSlide}>Next</button> */}
+              </div>
+              
+              <div></div>
+              </div>
+              );
+              };
+              
+              export default OurServiceAccordion;
+              
--- src/app/Component/Services/WebDev.jsx
+++ src/app/Component/Services/WebDev.jsx
@@ -1,141 +1,183 @@
 import React from "react";
 import Data from "../Constent/Priceinglist.json";
-import "../../Style/WebDev.css"
+import "../../Style/WebDev.css";
 import Image from "next/image";
-
 
 const WebDev = () => {
   const webDevelopmentData = Data["Web Development"];
 
   return (
     <div>
+      <div className="WebDevMainDivList ">
+        <div>
+          <h1 className="WebDevMainDivListH ">
+            Are you on the lookout for top-tier website development services to
+            craft online identity for your business or startup?{" "}
+          </h1>
+          <p className="WebDevMainDivListP ">
+            Waste no time and choose Paperfold – we pride ourselves on being a
+            leading website development company that ensures the best results at
+            affordable prices. We are dedicated to transforming your digital
+            aspirations into a tangible reality. Our seasoned team of developers
+            stands at the forefront of technological innovation, committed to
+            delivering websites that seamlessly blend aesthetics with
+            functionality. Our Expertise Encompasses: Custom Website
+            Development, E-Commerce Excellence, Responsive Design, Content
+            Management Systems (CMS), API Integration, Website Maintenance and
+            Support. Fast, Secure, SEO & Mobile Friendly Website Development
+            Services worldwide.
+          </p>
+        </div>
 
-    <div className="WebDevMainDivList ">
-<div>
-<h1 className="WebDevMainDivListH ">Are you on the lookout for top-tier website development services to craft online identity for your business or startup?  </h1>
-<p className="WebDevMainDivListP ">Waste no time and choose Paperfold – we pride ourselves on being a leading website development company that ensures the best results at affordable prices. We are dedicated to transforming your digital aspirations into a tangible reality. Our seasoned team of developers stands at the forefront of technological innovation, committed to delivering websites that seamlessly blend aesthetics with functionality. Our Expertise Encompasses: Custom Website Development, E-Commerce Excellence, Responsive Design, Content Management Systems (CMS), API Integration, Website Maintenance and Support. Fast, Secure, SEO & Mobile Friendly Website Development Services worldwide.</p>
-</div>
-  
-  
-<div className="EcomWebDivMain">
-      <div >
-      <Image
-         className="webDivImg"
-                height={330}
-                width={330}
-                alt=""
-                src="/EcomWebDev.png"
-              ></Image> 
-      </div>
-      <div className="EcomWebDivMainDiv2">
+        <div className="EcomWebDivMain">
+          <div>
+            <Image
+              className="webDivImg"
+              height={330}
+              width={330}
+              alt=""
+              src="/EcomWebDev.png"
+            ></Image>
+          </div>
+          <div className="EcomWebDivMainDiv2">
+            <h1 className="EcomWebDivMainDiv2H">Ecommerce Website</h1>
+            <p className="EcomWebDivMainDiv2P">
+              We offer a wide range of eCommerce website design & development
+              services. We have unmatched expertise in working on all leading
+              eCommerce platforms including Prestashop, Magento, OSCommerce,
+              VirtueMart, Zencart, Opencart, Ubercart, WooCommerce, BigCommerce,
+              Shopify, etc. At Paperfold Sequential we have a vast pool of
+              skilled and professional eCommerce website developers who have
+              great expertise in developing custom specifications, modules, and
+              user-centric shopping cart software.
+            </p>
+          </div>
+        </div>
 
-<h1 className="EcomWebDivMainDiv2H">Ecommerce Website</h1>
-<p className="EcomWebDivMainDiv2P">We offer a wide range of eCommerce website design & development services. We have unmatched expertise in working on all leading eCommerce platforms including Prestashop, Magento, OSCommerce, VirtueMart, Zencart, Opencart, Ubercart, WooCommerce, BigCommerce, Shopify, etc. At Paperfold Sequential we have a vast pool of skilled and professional eCommerce website developers who have great expertise in developing custom specifications, modules, and user-centric shopping cart software.</p>
-      </div>
-    </div>
+        <div className="EcomWebDivMain revdivWeb">
+          <div className="EcomWebDivMain2">
+            <h1 className="EcomWebDivMainH">Custom Web App Development</h1>
+            <p className="EcomWebDivMainP">
+              We, as a custom web app development company in India, catering to
+              thousands of businesses and their digital needs all across the
+              world. Along with our domain expertise, we have been working over
+              to help start-ups and businesses of varied industries to develop
+              B2B and B2C web applications. Our full-time custom web app
+              developers have been giving their best to successfully deliver the
+              projects to the clients.
+            </p>
+          </div>
+          <div>
+            <Image
+              className="webDivImg"
+              height={330}
+              width={330}
+              alt=""
+              src="/CustomWeb.png"
+            ></Image>
+          </div>
+        </div>
 
-    <div className="EcomWebDivMain">
- 
-      <div className="EcomWebDivMain2">
+        <div className="EcomWebDivMain">
+          <div>
+            <Image
+              className="webDivImg"
+              height={330}
+              width={330}
+              alt=""
+              src="/WebDev3Img.png"
+            ></Image>
+          </div>
+          <div className="EcomWebDivMainDiv2">
+            <h1 className="EcomWebDivMainDiv2H">Website Maintenance</h1>
+            <p className="EcomWebDivMainDiv2P">
+              Let our experts take care of your website maintenance needs, while
+              you take care of what’s more important for your business!
+              Proactive and comprehensive website maintenance solutions Expert
+              team of professionals dedicated to enhancing your website’s
+              performance Custom-made maintenance packages to suit your specific
+              needs Regular updates and security patches to keep your website
+              safe Timely backups and disaster recovery options for peace of
+              mind
+            </p>
+          </div>
+        </div>
 
-<h1 className="EcomWebDivMainH">Custom Web App Development</h1>
-<p className="EcomWebDivMainP">We, as a custom web app development company in India, catering to thousands of businesses and their digital needs all across the world. Along with our domain expertise, we have been working over to help start-ups and businesses of varied industries to develop B2B and B2C web applications. Our full-time custom web app developers have been giving their best to successfully deliver the projects to the clients.</p>
-      </div>
-      <div >
-      <Image
-         className="webDivImg"
-                height={330}
-                width={330}
-                alt=""
-                src="/EcomWebDev.png"
-              ></Image> 
-      </div>
-    </div>
-
-
-    <div className="EcomWebDivMain">
-      <div >
-      <Image
-         className="webDivImg"
-                height={330}
-                width={330}
-                alt=""
-                src="/WebDev3Img.png"
-              ></Image> 
-      </div>
-      <div className="EcomWebDivMainDiv2">
-
-<h1 className="EcomWebDivMainDiv2H">Website Maintenance</h1>
-<p className="EcomWebDivMainDiv2P">Let our experts take care of your website maintenance needs, while you take care of what’s more important for your business! Proactive and comprehensive website maintenance solutions Expert team of professionals dedicated to enhancing your website’s performance Custom-made maintenance packages to suit your specific needs Regular updates and security patches to keep your website safe Timely backups and disaster recovery options for peace of mind</p>
-      </div>
-    </div>
-  
-
-
-    <div className="WebMainDivWebDesign justify-evenly">
-        {/* <h1>{webDevelopmentData.name}</h1>
+        <div className="WebMainDivWebDesign justify-evenly">
+          {/* <h1>{webDevelopmentData.name}</h1>
             <p>{webDevelopmentData.content1}</p> */}
-        <div className="baseTableWebDev">
-          {/* Render base plan features */}
-          <h2 className="servicetableHeadingNew">{webDevelopmentData.BackEnd.planName}</h2>
-          <ul className="ml-4" >
-            {webDevelopmentData.BackEnd.features.map((feature, index) => (
-              <li key={index} className="LiImgBox">
-                {/* <span className="servicetableLi">{feature.text}</span> */}
-                <img src={feature.image} alt={feature.text} />
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="baseTableWebDev">
+            {/* Render base plan features */}
+            <h2 className="servicetableHeadingNew">
+              {webDevelopmentData.BackEnd.planName}
+            </h2>
+            <ul className="ml-4">
+              {webDevelopmentData.BackEnd.features.map((feature, index) => (
+                <li key={index} className="LiImgBox">
+                  {/* <span className="servicetableLi">{feature.text}</span> */}
+                  <img src={feature.image} alt={feature.text} />
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="baseTableWebDev">
-          {/* Render medium plan features */}
-          <h2 className="servicetableHeadingNew">{webDevelopmentData.FrontEnd.planName}</h2>
-          <ul className="ml-4">
-            {webDevelopmentData.FrontEnd.features.map((feature, index) => (
-              <li key={index} className="LiImgBox">
-                {/* <span className="servicetableLi" >{feature.text}</span> */}
-                <img src={feature.image} alt={feature.text} />
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="baseTableWebDev">
+            {/* Render medium plan features */}
+            <h2 className="servicetableHeadingNew">
+              {webDevelopmentData.FrontEnd.planName}
+            </h2>
+            <ul className="ml-4">
+              {webDevelopmentData.FrontEnd.features.map((feature, index) => (
+                <li key={index} className="LiImgBox">
+                  {/* <span className="servicetableLi" >{feature.text}</span> */}
+                  <img src={feature.image} alt={feature.text} />
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="baseTableWebDev">
-          {/* Render pro plan features */}
-          <h2 className="servicetableHeadingNew">{webDevelopmentData.CMS.planName}</h2>
-          <ul className="ml-4">
-            {webDevelopmentData.CMS.features.map((feature, index) => (
-              <li key={index} className="LiImgBox">
-                {/* <span className="servicetableLi" >{feature.text}</span> */}
-                <img src={feature.image} alt={feature.text} />
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="baseTableWebDev">
+            {/* Render pro plan features */}
+            <h2 className="servicetableHeadingNew">
+              {webDevelopmentData.CMS.planName}
+            </h2>
+            <ul className="ml-4">
+              {webDevelopmentData.CMS.features.map((feature, index) => (
+                <li key={index} className="LiImgBox">
+                  {/* <span className="servicetableLi" >{feature.text}</span> */}
+                  <img src={feature.image} alt={feature.text} />
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="baseTableWebDev">
-          {/* Render pro plan features */}
-          <h2 className="servicetableHeadingNew">{webDevelopmentData.ServerCloud.planName}</h2>
-          <ul className="ml-4" >
-            {webDevelopmentData.ServerCloud.features.map((feature, index) => (
-              <li key={index} className="LiImgBox">
-                {/* <span className="servicetableLi" >{feature.text}</span> */}
-                <img src={feature.image} alt={feature.text} />
-              </li>
-            ))}
-          </ul>
+          <div className="baseTableWebDev">
+            {/* Render pro plan features */}
+            <h2 className="servicetableHeadingNew">
+              {webDevelopmentData.ServerCloud.planName}
+            </h2>
+            <ul className="ml-4">
+              {webDevelopmentData.ServerCloud.features.map((feature, index) => (
+                <li key={index} className="LiImgBox">
+                  {/* <span className="servicetableLi" >{feature.text}</span> */}
+                  <img src={feature.image} alt={feature.text} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-
-     
       </div>
 
-    </div>
-
-    <div className="DesignDivWebnew">
+      <div className="DesignDivWebnew">
         <div className="DesignDivWeb2">
           <h1 className="DesignH"> Designing with emotional intelligence</h1>
-          <p className="DesignPa">Looking for a top web developer? Websites play an integral role in sales and marketing. A bespoke website can elevate your online presence, helping establish your brand’s credibility and increase business opportunities. Reach more people with a sophisticated website and work with a reliable web development partner.</p>
+          <p className="DesignPa">
+            Looking for a top web developer? Websites play an integral role in
+            sales and marketing. A bespoke website can elevate your online
+            presence, helping establish your brand’s credibility and increase
+            business opportunities. Reach more people with a sophisticated
+            website and work with a reliable web development partner.
+          </p>
         </div>
         <div>
           <Image
@@ -147,9 +189,6 @@ const WebDev = () => {
           ></Image>
         </div>
       </div>
-
-
-
 
       {/* Render additional services */}
       <div>
